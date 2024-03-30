eef28d00948820269c76db62c8babe03e5517952
Tzu-Ching Yang <shingekinocore@gmail.com>
2024-03-30 16:29:54 +0000

    Collect golden classes into same sub-package

--- src/test/scala/acal/lab05/Hw2/golden/GoldenCalculator.scala
+++ src/test/scala/acal/lab05/Hw2/golden/GoldenCalculator.scala
@@ -1,38 +1,7 @@
-package acal.lab05.Hw2
+package acal.lab05.Hw2.golden
 
-import scala.util.Try
 import scala.util.Random
 import scala.sys.process._
-
-class GoldenStack[T] {
-  var stack = List[T]()
-  var peak  = 0
-
-  /**
-   * Push an element at the front
-   */
-  def push(e: T) = {
-    stack = e :: stack
-    peak  = Math.max(peak, stack.size)
-  }
-
-  /**
-   * Pop the front element
-   */
-  def pop() = {
-    val ret = stack(0)
-    stack = stack.tail
-    ret
-  }
-
-  /**
-   * Peek the front element
-   */
-  def peek = stack(0)
-
-  def isEmpty = stack.isEmpty
-  def size    = stack.size
-}
 
 /**
  * A robust calculator, software modeling of
---
+++ src/test/scala/acal/lab05/Hw2/golden/GoldenStack.scala
@@ -0,0 +1,31 @@
+package acal.lab05.Hw2.golden
+
+class GoldenStack[T] {
+  var stack = List[T]()
+  var peak  = 0
+
+  /**
+   * Push an element at the front
+   */
+  def push(e: T) = {
+    stack = e :: stack
+    peak  = Math.max(peak, stack.size)
+  }
+
+  /**
+   * Pop the front element
+   */
+  def pop() = {
+    val ret = stack(0)
+    stack = stack.tail
+    ret
+  }
+
+  /**
+   * Peek the front element
+   */
+  def peek = stack(0)
+
+  def isEmpty = stack.isEmpty
+  def size    = stack.size
+}
